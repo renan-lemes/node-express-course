41ac0c8f9bbe8e4842f1fe30fe11be35e9c9a007
renanll <renan_ll@hotmail.com>
2021-10-31 19:25:06 -0300

    add dynamic GET route

--- server.js
+++ server.js
@@ -2,16 +2,25 @@ const express = require('express'); //da acesso a bliblioteca expressa na pasta 
 const app = express(); // instancia um construtor
 
 const mockUserData = [
-    {name : 'Mark' },
-    {name : 'Jill'}
+    { name: 'Mark' },
+    { name: 'Jill' }
 ]
-app.get('/users' , function(req, res){
+app.get('/users/:id', function (req, res) {// req para selecionar ou acessar o valor  // ja res inserir o inserção
+    console.log(req.params.id)
     res.json({
-        success : true,
-        message : 'successfully got users. Nice!', 
+        success: true,
+        message: 'successfully got users. Nice!',
         users: mockUserData
     })
 
+})
+app.get('/users/:id', function (req, res) {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
 })
 
 
